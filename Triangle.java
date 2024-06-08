a2effecdb0431e08fd94f90e1f4e2c4e5be73a2c
gayatrimetkar <158744131+gayatrimetkar@users.noreply.github.com>
2024-06-08 22:13:14 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle 
+{
+	public static void main(String args[ ])
+	{
+		int h= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+		int tri= h*b/2;
+		System.out.println("Triangle" +tri);
+	}
+}
