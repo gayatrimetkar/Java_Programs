e83faf6574502b9caaad053d76abbbcc38a3fc5e
gayatrimetkar <158744131+gayatrimetkar@users.noreply.github.com>
2024-06-08 21:59:28 +0530

    Add files via upload

---
+++ postive_negative_no.java
@@ -0,0 +1,15 @@
+public class postive_negative
+{
+	public static void main(String args[])
+	{
+		int n= Integer.parseInt(args[0]);
+		if (n>0)
+		{
+			System.out.println(n+ "is postive");
+		}
+		else
+		{
+			System.out.println(n+ "is negative");
+		}
+	}
+}
